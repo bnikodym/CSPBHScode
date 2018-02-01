7ca636c2c15f8bc69e7df1e13410cea3bcffa9ee
bnikodym <30477964+bnikodym@users.noreply.github.com>
2018-02-01 07:59:58 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -2,5 +2,5 @@
 $host = "localhost";
 $dbname ="artgallery";
 $username = "bnikodym";
-$password = "# Remove git branch from shell prompt. sed -i -e "s/$(__git_ps1 " (%s)")//" ../.bashrc# Apply the new .bashrc to the current shell (This doesn't work from the script)# source ../.bashrc vickijvickijo53";
+$password = "";
 ?>
